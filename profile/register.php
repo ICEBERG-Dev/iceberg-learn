0b5c44abe5d6db1131f9cf1fe662b25af858b6f5
DenFaust <dpinyagin@inbox.ru>
2023-02-15 10:39:31 +0600

    feat: add form log & reg

---
+++ profile/register.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/profile/style/login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;1,400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@500&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="">
+    <title>IceBergLogin</title>
+
+</head>
+<body>
+
+
+<header>
+    <div class="container">
+        <div class="header-container">
+            <a href="https://www.google.com/search?client=opera&q=translate&sourceid=opera&ie=UTF-8&oe=UTF-8" target="_blank" class="mainlogo"><img src="/assets/images/mainlogo.png" alt="" width="70" ></a>
+            <div>
+                <a class="HeaderButtons" href="">About</a>
+                <a class="HeaderButtons" href="">Blog</a>
+                <a class="HeaderButtons" href="">Pages</a>
+            </div>
+            <div>
+                <a class="HeaderButtons" href="">SignIn</a>
+                <a class="HeaderButtons" href="">Register</a>
+            </div>
+
+        </div>
+    </div>
+</header>
+
+<main>
+
+    <div class="circle"> <h1>Welcome to <span class="color-text">Ice</span>Berg Group </h1>
+
+    </div>
+    <div class="register-form-container">
+        <h2 class="form-title">
+            Login In
+        </h2>
+        <div class="form-fields">
+            <div class="form-field">
+                <input type="text" placeholder="Email">
+                <div class="form-field">
+                    <input type="text" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-buttons">
+                <button class="button">Register</button>
+                <div class="divider">Or continue with</div>
+                <a href="" class="logo"><img src="/profile/images/googlelogo.png" alt="" width="30"></a>
+                <a href="" class="logo"><img src="/profile/images/applelogo.png" alt="" width="30"></a>
+                <a href="" class="logo"><img src="/profile/images/facebook.png" alt="" width="30"></a>
+
+            </div>
+        </div>
+
+</main>
+
+
+
+</body>
+</html>
