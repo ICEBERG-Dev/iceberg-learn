1435903cb598a2e7332854dac66c2d5f01e79dcc
webyzus <workyzus@gmail.com>
2023-02-19 14:38:27 +0600

    switch icon

--- profile/scripts.js
+++ profile/scripts.js
@@ -1,6 +1,9 @@
 let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
+let switchIcon = document.querySelector('.fa');
 themeButton.onclick = function () {
     page.classList.toggle('whitetheme')
+    switchIcon.classList.toggle('fa-moon')
     page.classList.toggle('darktheme')
+    switchIcon.classList.toggle('fa-lightbulb')
 };
