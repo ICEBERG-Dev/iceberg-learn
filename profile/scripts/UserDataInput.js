5ea704e7fd784473644eddc91d369f566cc704ac
webyzus <workyzus@gmail.com>
2023-02-20 10:57:15 +0600

    login password check

--- profile/scripts/UserDataInput.js
+++ profile/scripts/UserDataInput.js
@@ -2,7 +2,10 @@ let datauser = [
     {login: 'Login', value: []},
     {password: 'Password', value: []}
 ];
-
+const bdUser = {
+    login: 'admin',
+    password: '12345'
+}
 document.querySelector('.button-user-data').addEventListener('click', ()=>{
     let user_login = document.querySelector('.user-login-id').value;
     let user_password = document.querySelector('.user-password-id').value;
@@ -10,5 +13,14 @@ document.querySelector('.button-user-data').addEventListener('click', ()=>{
     datauser[1].value.push(user_password);
     console.log(`Логин пользователя: ${datauser[0].value}`);
     console.log(`Пароль пользователя: ${datauser[1].value}`);
+    if (user_login === bdUser.login && user_password === bdUser.password) {
+        window.location.href = 'http://ice-todo/';
+    } else {
+        let alert = document.createElement('p')
+        alert.classList.add('alert')
+        alert.innerHTML = 'Логин или пароль не верны'
+        document.querySelector('.login_body-footer').insertAdjacentElement('beforebegin', alert);
+        setTimeout(()=>document.querySelector('.alert').remove(), 3000)
+    }
 })
 
