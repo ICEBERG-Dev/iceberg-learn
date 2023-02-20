671bba7bae41a54e95cb746b7ef26ea867577d6d
webyzus <workyzus@gmail.com>
2023-02-20 18:52:46 +0600

    task page v.2

--- tasks/scripts/TaskScripts.js
+++ tasks/scripts/TaskScripts.js
@@ -9,17 +9,25 @@ addTaskButton.addEventListener('click', ()=>{
     if(document.querySelectorAll('.task_message').length == 0) {
         document.querySelector('.container_task').insertAdjacentElement('beforeend', userTask);
     } else if(document.querySelectorAll('.task_message').length >= 1){
-        let userTaskAnswer = document.createElement('p');
+        if (document.querySelector('.task_message').value == '') {alert('Пустые данные')
+            return}
+        let userTaskAnswer = document.createElement('div');
         userTaskAnswer.classList.add('user_task_answer');
         userTaskAnswer.innerHTML = document.querySelector('.task_message').value;
+        let removeButton = document.createElement('button');
+        removeButton.classList.add('remove_button');
+        let removeIcon = document.createElement('i');
+        removeIcon.classList.add('fa-solid','fa-trash');
         document.querySelector('.container_task').insertAdjacentElement('beforeend', userTaskAnswer);
+        document.querySelector('.user_task_answer').insertAdjacentElement('beforeend', removeButton);
+        document.querySelector('.remove_button').insertAdjacentElement('beforeend', removeIcon);
         document.querySelector('.task_message').remove();
 
         }
 
     });
 
-removeButton.addEventListener('click', ()=>{;
+removeButton.addEventListener('click', ()=>{
     if(document.querySelectorAll('.user_task_answer').length >= 1 ){
         document.querySelector('.user_task_answer').remove();
     } else {
