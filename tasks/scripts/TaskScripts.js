ed047a8739dd3c88647ffa3e92a8b9dc2638d15b
webyzus <workyzus@gmail.com>
2023-02-21 15:20:51 +0600

    task page v.3

--- tasks/scripts/TaskScripts.js
+++ tasks/scripts/TaskScripts.js
@@ -1,36 +1,28 @@
 let addTaskButton = document.querySelector('.add_task_button');
 let removeButton = document.querySelector('.remove_button');
 
-addTaskButton.addEventListener('click', ()=>{
+addTaskButton.addEventListener('click', () => {
     let userTask = document.createElement('textarea');
     userTask.classList.add('task_message');
     userTask.setAttribute('rows', '5');
     userTask.setAttribute('cols', '33');
-    if(document.querySelectorAll('.task_message').length == 0) {
+    if (document.querySelectorAll('.task_message').length == 0) {
         document.querySelector('.container_task').insertAdjacentElement('beforeend', userTask);
-    } else if(document.querySelectorAll('.task_message').length >= 1){
-        if (document.querySelector('.task_message').value == '') {alert('Пустые данные')
-            return}
+    } else if (document.querySelectorAll('.task_message').length >= 1) {
+        if (document.querySelector('.task_message').value == '') {
+            alert('Пустые данные')
+            return
+        }
         let userTaskAnswer = document.createElement('div');
         userTaskAnswer.classList.add('user_task_answer');
-        userTaskAnswer.innerHTML = document.querySelector('.task_message').value;
-        let removeButton = document.createElement('button');
-        removeButton.classList.add('remove_button');
-        let removeIcon = document.createElement('i');
-        removeIcon.classList.add('fa-solid','fa-trash');
+        userTaskAnswer.innerHTML = `
+        <p>${document.querySelector('.task_message').value}</p>
+        <button class="remove_button"><i class="fa-solid fa-trash fa-xl"></i></button>`
+
         document.querySelector('.container_task').insertAdjacentElement('beforeend', userTaskAnswer);
-        document.querySelector('.user_task_answer').insertAdjacentElement('beforeend', removeButton);
-        document.querySelector('.remove_button').insertAdjacentElement('beforeend', removeIcon);
         document.querySelector('.task_message').remove();
 
-        }
-
-    });
-
-removeButton.addEventListener('click', ()=>{
-    if(document.querySelectorAll('.user_task_answer').length >= 1 ){
-        document.querySelector('.user_task_answer').remove();
-    } else {
-        return;
     }
-})
+
+});
+
