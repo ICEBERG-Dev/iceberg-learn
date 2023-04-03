be3374a2f5da400297774f53ae23a69eff1c06cf
190103494 <63901134+190103494@users.noreply.github.com>
2023-04-03 16:06:25 +0600

    feat: modified index.php && create style.css

--- index.php
+++ index.php
@@ -5,8 +5,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <h1>Taskman</h1>
+<div class="page">
+    <div class="sidebar">
+        <div class="sidebar_images">
+            <img src="./assets/image/fluent_arrow-exit-20-filled.png" alt="">
+            <img src="./assets/image/home-active.png" class="picture" alt="">
+            <img src="./assets/image/ph_users.png"  alt="">
+        </div>
+
+        <div class="sidebar_line"></div>
+
+
+        <div class="sidebar_images">
+            <img src="./assets/image/fluent_dark-theme-20-filled.png" alt="">
+            <img src="./assets/image/Vector@2x.png" alt="">
+
+        </div>
+    </div>
+
+
+    <div class="content">
+        <div class="content_header"></div>
+
+        <div class="content_main">
+            <div class="content_info"></div>
+            <div class="content_aside"></div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
 </body>
 </html>
